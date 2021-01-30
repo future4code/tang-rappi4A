32d737026343ed0d34911bbf9d339157e2b795cc
Luana Alencar <luana.eclips@gmail.com>
2021-01-29 21:31:52 -0300

    home login e cadastro

--- src/screens/HomeScreen/styled.js
+++ src/screens/HomeScreen/styled.js
@@ -1,1 +1,19 @@
+import { colors } from '@material-ui/core';
+import { red } from '@material-ui/core/colors';
 import styled from 'styled-components';
+
+export const HomeContainer = styled.div`
+width: 360px;
+height: 640px;
+padding: 288px 117px 287px;
+background-color: #e86e5a;
+text-align: center;
+
+`
+export const Logo = styled.div`
+width: 126px;
+height: 65px;
+object-fit: contain;
+
+
+` 
--- src/screens/LoginScreen/index.js
+++ src/screens/LoginScreen/index.js
@@ -1,5 +1,32 @@
 import React from 'react';
+import Logo from "../../assets/Rappi4-logo-fundo-vermelho.svg"
+import {Button} from "@material-ui/core";
+import { LoginContainer, FormsContainer} from './styled';
+import { LoginForm } from './LoginForm';
+import { useHistory } from 'react-router-dom';
 
 export const LoginScreen = () => {
-  return <div>LoginScreen</div>
+    const history = useHistory()
+
+
+  return (
+  <LoginContainer>
+    <FormsContainer>
+      <img src ={Logo}/>
+        <p>Entrar</p>
+          <LoginForm/>
+
+           <Button
+            onClick={() => goToSignUpScreen(history)}
+            variant={"contained"}
+            margin={"normal"}
+            color={"primary"}
+            fullWidth
+         >
+            Não possui cadastro? Clique aqui.
+          </Button>
+        </FormsContainer>
+      
+      </LoginContainer>
+  )
 }
